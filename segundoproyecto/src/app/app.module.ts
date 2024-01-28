5895f4e7e863dfb426679d92246be64320d0976b
= <info@josevicentecarratala.com>
2024-01-28 11:15:10 +0100

    clase 5

--- segundoproyecto/src/app/app.module.ts
+++ segundoproyecto/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
+import { MenuComponent } from './menu/menu.component';
+import { PancartaComponent } from './pancarta/pancarta.component';
+import { DestacadosComponent } from './destacados/destacados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
     PrincipalComponent,
-    PiedepaginaComponent
+    PiedepaginaComponent,
+    MenuComponent,
+    PancartaComponent,
+    DestacadosComponent
   ],
   imports: [
     BrowserModule,
